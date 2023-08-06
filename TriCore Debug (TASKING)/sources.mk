b6de1665a1e1714e026fa53ba438eb157fd86efe
abhiram-pavuluri <109450848+abhiram-pavuluri@users.noreply.github.com>
2023-08-06 11:53:13 +0530

    Add files via upload

---
+++ TriCore Debug (TASKING)/sources.mk
@@ -0,0 +1,51 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+C++_SRCS := 
+CC_SRCS := 
+CPP_SRCS := 
+CXX_SRCS := 
+C_SRCS := 
+C_UPPER_SRCS := 
+OBJ_SRCS := 
+O_SRCS := 
+SRC_SRCS := 
+C++_DEPS := 
+CC_DEPS := 
+COMPILED_SRCS := 
+CPP_DEPS := 
+CXX_DEPS := 
+C_DEPS := 
+C_UPPER_DEPS := 
+EXECUTABLES := 
+OBJS := 
+SECONDARY_SIZE := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+Configurations/Debug \
+Configurations \
+Libraries/Infra/Platform/Tricore/Compilers \
+Libraries/Infra/Ssw/TC39B/Tricore \
+Libraries/Service/CpuGeneric/StdIf \
+Libraries/iLLD/TC39B/Tricore/Asclin/Asc \
+Libraries/iLLD/TC39B/Tricore/Asclin/Std \
+Libraries/iLLD/TC39B/Tricore/Cpu/Irq \
+Libraries/iLLD/TC39B/Tricore/Cpu/Std \
+Libraries/iLLD/TC39B/Tricore/Cpu/Trap \
+Libraries/iLLD/TC39B/Tricore/Evadc/Adc \
+Libraries/iLLD/TC39B/Tricore/Evadc/Std \
+Libraries/iLLD/TC39B/Tricore/Gpt12/Std \
+Libraries/iLLD/TC39B/Tricore/Gtm/Std \
+Libraries/iLLD/TC39B/Tricore/Mtu/Std \
+Libraries/iLLD/TC39B/Tricore/Pms/Std \
+Libraries/iLLD/TC39B/Tricore/Port/Std \
+Libraries/iLLD/TC39B/Tricore/Scu/Std \
+Libraries/iLLD/TC39B/Tricore/Src/Std \
+Libraries/iLLD/TC39B/Tricore/Stm/Std \
+Libraries/iLLD/TC39B/Tricore/_Impl \
+Libraries/iLLD/TC39B/Tricore/_Lib/DataHandling \
+Libraries/iLLD/TC39B/Tricore/_PinMap \
+
---
+++ TriCore Debug (TASKING)/subdir.mk
@@ -0,0 +1,130 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ADC_Single_Channel.c \
+../Cpu0_Main.c \
+../Cpu1_Main.c \
+../Cpu2_Main.c \
+../Cpu3_Main.c \
+../Cpu4_Main.c \
+../Cpu5_Main.c \
+../GPT12_Timer_Interrupt.c \
+../UART_VCOM.c 
+
+COMPILED_SRCS += \
+./ADC_Single_Channel.src \
+./Cpu0_Main.src \
+./Cpu1_Main.src \
+./Cpu2_Main.src \
+./Cpu3_Main.src \
+./Cpu4_Main.src \
+./Cpu5_Main.src \
+./GPT12_Timer_Interrupt.src \
+./UART_VCOM.src 
+
+C_DEPS += \
+./ADC_Single_Channel.d \
+./Cpu0_Main.d \
+./Cpu1_Main.d \
+./Cpu2_Main.d \
+./Cpu3_Main.d \
+./Cpu4_Main.d \
+./Cpu5_Main.d \
+./GPT12_Timer_Interrupt.d \
+./UART_VCOM.d 
+
+OBJS += \
+./ADC_Single_Channel.o \
+./Cpu0_Main.o \
+./Cpu1_Main.o \
+./Cpu2_Main.o \
+./Cpu3_Main.o \
+./Cpu4_Main.o \
+./Cpu5_Main.o \
+./GPT12_Timer_Interrupt.o \
+./UART_VCOM.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.src: ../%.c subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING C/C++ Compiler'
+	cctc -cs --dep-file="$(basename $@).d" --misrac-version=2004 -D__CPU__=tc39xb "-fC:/Users/chandrasekhar y/Documents/Task_3_(2)[1]/Task 3/TriCore Debug (TASKING)/TASKING_C_C___Compiler-Include_paths.opt" --iso=99 --c++14 --language=+volatile --exceptions --anachronisms --fp-model=3 -O0 --tradeoff=4 --compact-max-size=200 -g -Wc-w544 -Wc-w557 -Ctc39xb -Y0 -N0 -Z0 -o "$@" "$<" && \
+	if [ -f "$(basename $@).d" ]; then sed.exe -r  -e 's/\b(.+\.o)\b/\/\1/g' -e 's/\\/\//g' -e 's/\/\//\//g' -e 's/"//g' -e 's/([a-zA-Z]:\/)/\L\1/g' -e 's/\d32:/@TARGET_DELIMITER@/g; s/\\\d32/@ESCAPED_SPACE@/g; s/\d32/\\\d32/g; s/@ESCAPED_SPACE@/\\\d32/g; s/@TARGET_DELIMITER@/\d32:/g' "$(basename $@).d" > "$(basename $@).d_sed" && cp "$(basename $@).d_sed" "$(basename $@).d" && rm -f "$(basename $@).d_sed" 2>/dev/null; else echo 'No dependency file to process';fi
+	@echo 'Finished building: $<'
+	@echo ' '
+
+ADC_Single_Channel.o: ./ADC_Single_Channel.src subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING Assembler'
+	astc -Og -Os --no-warnings= --error-limit=42 -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Cpu0_Main.o: ./Cpu0_Main.src subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING Assembler'
+	astc -Og -Os --no-warnings= --error-limit=42 -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Cpu1_Main.o: ./Cpu1_Main.src subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING Assembler'
+	astc -Og -Os --no-warnings= --error-limit=42 -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Cpu2_Main.o: ./Cpu2_Main.src subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING Assembler'
+	astc -Og -Os --no-warnings= --error-limit=42 -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Cpu3_Main.o: ./Cpu3_Main.src subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING Assembler'
+	astc -Og -Os --no-warnings= --error-limit=42 -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Cpu4_Main.o: ./Cpu4_Main.src subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING Assembler'
+	astc -Og -Os --no-warnings= --error-limit=42 -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Cpu5_Main.o: ./Cpu5_Main.src subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING Assembler'
+	astc -Og -Os --no-warnings= --error-limit=42 -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+GPT12_Timer_Interrupt.o: ./GPT12_Timer_Interrupt.src subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING Assembler'
+	astc -Og -Os --no-warnings= --error-limit=42 -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+UART_VCOM.o: ./UART_VCOM.src subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: TASKING Assembler'
+	astc -Og -Os --no-warnings= --error-limit=42 -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
+clean: clean--2e-
+
+clean--2e-:
+	-$(RM) ./ADC_Single_Channel.d ./ADC_Single_Channel.o ./ADC_Single_Channel.src ./Cpu0_Main.d ./Cpu0_Main.o ./Cpu0_Main.src ./Cpu1_Main.d ./Cpu1_Main.o ./Cpu1_Main.src ./Cpu2_Main.d ./Cpu2_Main.o ./Cpu2_Main.src ./Cpu3_Main.d ./Cpu3_Main.o ./Cpu3_Main.src ./Cpu4_Main.d ./Cpu4_Main.o ./Cpu4_Main.src ./Cpu5_Main.d ./Cpu5_Main.o ./Cpu5_Main.src ./GPT12_Timer_Interrupt.d ./GPT12_Timer_Interrupt.o ./GPT12_Timer_Interrupt.src ./UART_VCOM.d ./UART_VCOM.o ./UART_VCOM.src
+
+.PHONY: clean--2e-
+
